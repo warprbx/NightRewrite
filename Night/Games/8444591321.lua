9f0f06ce9a7a26f0633eee65d7abde3e862d4449
kiurgjirkgk <111064086+kiurgjirkgk@users.noreply.github.com>
2025-01-12 22:21:07 +0000

    Add files via upload

---
+++ Night/Games/8444591321.lua
@@ -0,0 +1,3 @@
+local Night = getgenv().Night
+Night.Assets.Config.Load("6872274481", "Game")
+Night.Assets.Main.Load("Night/Games/6872274481.lua", "https://raw.githubusercontent.com/warprbx/NightRewrite/refs/heads/main/Night/Games/6872274481.lua")
