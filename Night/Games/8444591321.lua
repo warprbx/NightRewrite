56336beb0e974a98af5b33549cdc1ccff7f0df25
luc <163360653+warprbx@users.noreply.github.com>
2025-01-21 08:37:50 -0800

    Update 8444591321.lua

--- Night/Games/8444591321.lua
+++ Night/Games/8444591321.lua
@@ -1,3 +1,4 @@
 local Night = getgenv().Night
+Night.GameSave = "6872274481"
 Night.Assets.Config.Load("6872274481", "Game")
 Night.Assets.Main.Load("Night/Games/6872274481.lua", "https://raw.githubusercontent.com/warprbx/NightRewrite/refs/heads/main/Night/Games/6872274481.lua")
