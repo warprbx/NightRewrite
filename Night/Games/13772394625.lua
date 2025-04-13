b9a86155a7e7093d9dbc0770d9bcd38b5179ec8c
kiurgjirkgk <111064086+kiurgjirkgk@users.noreply.github.com>
2025-04-13 16:31:11 +0100

    Update 13772394625.lua

--- Night/Games/13772394625.lua
+++ Night/Games/13772394625.lua
@@ -1,11 +1,15 @@
 if not Night.Premium then
-    Night.Assets.Notifications.Send({
-        Description = "This is a premium only game, you can buy premium here: https://discord.gg/w8RnktNAnn, copied to clipboard",
-        Duration = 5
-    })
-    if setclipboard then
-        setclipboard("https://discord.gg/w8RnktNAnn")
-    elseif not setclipboard and toclipboard then
-        toclipboard("https://discord.gg/w8RnktNAnn")
-    end
+    task.spawn(function()
+        repeat task.wait() until Night.Loaded
+        task.wait(0.35)
+        Night.Assets.Notifications.Send({
+            Description = "This is a premium only game, you can buy premium here: https://discord.gg/w8RnktNAnn, copied to clipboard",
+            Duration = 5
+        })
+        if setclipboard then
+            setclipboard("https://discord.gg/w8RnktNAnn")
+        elseif not setclipboard and toclipboard then
+            toclipboard("https://discord.gg/w8RnktNAnn")
+        end
+    end)
 end
